3e2118affd0ac48afcf3c5b2563ea7031c422057
Gil Alpert <alpertgil@gmail.com>
2023-01-26 01:26:50 +0200

    Doctor search functionality added, need to prevent form resubmission on refresh message.

--- src/main/java/DoctorListBean.java
+++ src/main/java/DoctorListBean.java
@@ -2,15 +2,42 @@ import com.maorgil.hospitalappointmentsystem.DBHandler;
 import com.maorgil.hospitalappointmentsystem.entity.DoctorsEntity;
 
 import javax.faces.bean.ManagedBean;
+import java.util.Comparator;
 import java.util.List;
 
 @ManagedBean(name = "doctorsListBean")
 public class DoctorListBean {
-    private final List<DoctorsEntity> doctors;
+    private List<DoctorsEntity> doctors = new DBHandler().getDoctors();
+    public String searchTerm = "";
+    public String output;
 
-    public DoctorListBean() {
-        DBHandler dbHandler = new DBHandler();
-        doctors = dbHandler.getDoctors();
+    public void setInput(String s) {
+        searchTerm = s;
+    }
+
+    public String getInput() {
+        return searchTerm;
+    }
+
+    public void submit() {
+        doctors = new DBHandler().getDoctorsBySearch(searchTerm);
+        // sort by first name then last name
+        doctors.sort(Comparator.comparing(DoctorsEntity::getFirstName).thenComparing(DoctorsEntity::getLastName));
+    }
+
+    public String getOutput() {
+        StringBuilder sb = new StringBuilder();
+        for (DoctorsEntity doctor : doctors) {
+            sb.append("<ul>" +
+                    "<li>\n" +
+                    "<label class=\"title\">" + doctor.getTitle() + "</label>\n" +
+                    "<br/>" +
+                    "<label class=\"about\">" + doctor.getAbout() + "</label>\n" +
+                    "</li>" +
+                    "</ul>");
+        }
+
+        return sb.toString();
     }
 
     /**
