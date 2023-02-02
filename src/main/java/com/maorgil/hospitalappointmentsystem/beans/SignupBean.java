786edda1d4432e72f73aeb6da4d78008f0c6ad1c
Maor Shweky <maor5398@gmail.com>
2023-02-02 19:11:54 +0200

    Fix adding to DB error, sign up works now

--- src/main/java/com/maorgil/hospitalappointmentsystem/beans/SignupBean.java
+++ src/main/java/com/maorgil/hospitalappointmentsystem/beans/SignupBean.java
@@ -4,9 +4,6 @@ import com.maorgil.hospitalappointmentsystem.DBHandler;
 import com.maorgil.hospitalappointmentsystem.entity.UsersEntity;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
-import javax.faces.context.FacesContext;
-import java.io.IOException;
 import java.util.Date;
 
 @ManagedBean(name = "signupBean")
@@ -14,20 +11,15 @@ public class SignupBean {
 //    private UsersEntity user;
 
     // user properties
-    private String id;
-    private String firstName;
-    private String lastName;
+    private String id = "";
+    private String firstName = "";
+    private String lastName = "";
 
-    private String password;
-    private String phoneNumber;
-    private String email;
+    private String password = "";
+    private String phoneNumber = "";
+    private String email = "";
     private Date birthDate;
-    //private boolean isAdmin;
-
-//    public SignupBean() {
-//        initUserData();
-//    }
-
+    private String output = "";
     public String getId() {
         return id;
     }
@@ -53,15 +45,20 @@ public class SignupBean {
     }
 
     public String getPassword() {return password; }
+    public String getOutput() {
+        return output;
+    }
 
     public void setPassword(String password) { this.password = password; }
     public String getPhoneNumber() {
         // insert separators in the right spots
-        return phoneNumber;//phoneNumber.substring(0, 3) + "-" + phoneNumber.substring(3, 6) + "-" + phoneNumber.substring(6);
+        if (!phoneNumber.isEmpty())
+            return phoneNumber.substring(0, 3) + "-" + phoneNumber.substring(3, 6) + "-" + phoneNumber.substring(6);
+        return "";
     }
 
     public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;//phoneNumber.substring(0,3) + phoneNumber.substring(4,7) + phoneNumber.substring(8);
+        this.phoneNumber = phoneNumber.substring(0,3) + phoneNumber.substring(4,7) + phoneNumber.substring(8);
     }
 
     public String getEmail() {
@@ -80,37 +77,18 @@ public class SignupBean {
         this.birthDate = birthDate;
     }
 
-//    public String isAdminStr() {
-//        return isAdmin ? "Admin" : "User";
-//    }
-//
-//    public String isUser() {
-//        return isAdmin ? "false" : "true";
-//    }
-
-//    public String isPreviewMode() {
-//        return previewMode ? "true" : "false";
-//    }
-//
-//    public String getStyleClass(boolean isEditable) {
-//        return isEditable ? "" : "readonly";
-//    }
-//
-    /**
-     * Switch to edit mode (where some fields are editable) and reload the page
-     * @return the page to redirect to (same page with reload of the bean)
-     */
-//    public String edit() {
-//        previewMode = false;
-//        return "myprofile.xhtml?faces-redirect=true";
-//    }
-
     /**
      * persist the data to the DB and switch to back to preview mode, reload the page
      * @return the page to redirect to (same page with reload of the bean)
      */
     public String submit() {
         UsersEntity user = new UsersEntity(); //id, firstName, lastName, new java.sql.Date(birthDate.getTime()), password, phoneNumber, email, false);
+        DBHandler dbHandler = new DBHandler();
+
+        if (dbHandler.getUserById(id) != null) {
+            output = "Id already exist";
+            return "";
+        }
         // persist changes to DB
         user.setId(id);
         user.setFirstName(firstName);
@@ -121,30 +99,12 @@ public class SignupBean {
         user.setBirthDate(new java.sql.Date(birthDate.getTime()));
         user.setAdmin(false);
 
-        if (!new DBHandler().persistEntity(user, UsersEntity.class, user.getId())) {
+        if (!dbHandler.persistEntity(user, UsersEntity.class, user.getId())) {
             // init before sending in case of a DB error
-            //initUserData();
-            return "Signup.xhtml?faces-redirect=true";
+            output = "Error while sending data to DB";
+            return "";
         }
 
         return "index.xhtml?faces-redirect=true";
     }
-
-//    public void initUserData() {
-//        getLoggedInId();
-//        user = new DBHandler().getUserById(loggedInId);
-//        id = user.getId();
-//        firstName = user.getFirstName();
-//        lastName = user.getLastName();
-//        phoneNumber = user.getPhoneNumber();
-//        email = user.getEmail();
-//        birthDate = user.getBirthDate();
-//        isAdmin = user.isAdmin();
-//    }
-
-//    private void getLoggedInId() {
-//        LoginBean instance = LoginBean.getInstance();
-//        if (instance != null && instance.isLoggedIn())
-//            loggedInId = instance.getId();
-//    }
 }
