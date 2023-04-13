0f942c9d8027d85e687240edb7199e99625feb60
Gil Alpert <alpertgil@gmail.com>
2023-04-13 13:42:08 +0300

    Added edit appointment description feature

--- src/main/java/com/maorgil/hospitalappointmentsystem/beans/ProfileDoctorBean.java
+++ src/main/java/com/maorgil/hospitalappointmentsystem/beans/ProfileDoctorBean.java
@@ -2,6 +2,7 @@ package com.maorgil.hospitalappointmentsystem.beans;
 
 import com.maorgil.hospitalappointmentsystem.DBHandler;
 import com.maorgil.hospitalappointmentsystem.entity.AppointmentsEntity;
+import org.primefaces.PrimeFaces;
 
 import javax.faces.bean.ManagedBean;
 import java.util.List;
@@ -9,7 +10,9 @@ import java.util.List;
 @ManagedBean(name = "profileDoctorBean")
 public class ProfileDoctorBean {
     private final String id; // user id but the user is a doctor
-    private final DBHandler dbHandler = DBHandler.getInstance();
+
+    private static AppointmentsEntity selectedAppointment;
+    private static String description = "";
 
     public ProfileDoctorBean() {
         // get the logged-in user id from the login bean
@@ -17,14 +20,39 @@ public class ProfileDoctorBean {
     }
 
     public String getLastName() {
-        return dbHandler.getDoctorById(id).getLastName();
+        return DBHandler.getInstance().getDoctorById(id).getLastName();
     }
 
     public int getMaxDoctorAppointments() {
         return DBHandler.MAX_RESULTS_DOCTOR_UPCOMING_APPOINTMENTS;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        ProfileDoctorBean.description = description;
+    }
+
     public List<AppointmentsEntity> getUpcomingAppointments() {
-        return dbHandler.getUpcomingAppointmentsByDoctorId(id);
+        return DBHandler.getInstance().getUpcomingAppointmentsByDoctorId(id);
+    }
+
+    public void onEditDesc(AppointmentsEntity appointment) {
+        selectedAppointment = appointment;
+        description = selectedAppointment.getDescription() == null ? "" : selectedAppointment.getDescription();
+
+        // update with ajax
+        PrimeFaces.current().ajax().update("descEditPopup");
+
+        // show popup wanting description
+        PrimeFaces.current().executeScript("PF('descEditPopup').show()");
+    }
+
+    public void saveDescription() {
+        selectedAppointment.setDescription(description);
+        DBHandler.getInstance().persistEntity(selectedAppointment, AppointmentsEntity.class, selectedAppointment.getId());
+        PrimeFaces.current().ajax().update("dd" + selectedAppointment.getId());
     }
 }
